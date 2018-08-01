28bea066d3e023aa6d8bfedb682c9586ddd3b3bc
LBH-fschultesz <ferenc.schultesz@hackney.gov.uk>
2018-08-01 08:48:02 +0100

    Update FakeWorkOrdersService.cs
    Fixed indentation

--- HackneyRepairs/Services/FakeWorkOrdersService.cs
+++ HackneyRepairs/Services/FakeWorkOrdersService.cs
@@ -1,23 +1,23 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 using HackneyRepairs.Entities;
 using HackneyRepairs.Interfaces;
 
 namespace HackneyRepairs.Services
 {
-	public class FakeWorkOrdersService : IHackneyWorkOrdersService
+    public class FakeWorkOrdersService : IHackneyWorkOrdersService
     {      
-		public Task<WorkOrderEntity> GetWorkOrderByReference(string reference)
-		{
-			if (string.Equals(reference, "9999999999"))
-			{
-				return Task.Run(() => (WorkOrderEntity)null);
-			}
-			var workOrderEntity = new WorkOrderEntity
-			{
-				wo_ref = reference
-			};
-			return Task.Run(() => workOrderEntity);
-		}
-	}
+	public Task<WorkOrderEntity> GetWorkOrderByReference(string reference)
+	{
+	    if (string.Equals(reference, "9999999999"))
+	    {
+	        return Task.Run(() => (WorkOrderEntity)null);
+	    }
+	    var workOrderEntity = new WorkOrderEntity
+	    {
+	        wo_ref = reference
+	    };
+	    return Task.Run(() => workOrderEntity);
+    	}
+    }
 }
