441393f26ecc281a6ba50e6602244837ed7855b4
LBH-fschultesz <ferenc.schultesz@hackney.gov.uk>
2018-08-01 08:44:19 +0100

    Update WorkOrdersController.cs
    Fixed indentation

--- HackneyRepairs/Controllers/WorkOrdersController.cs
+++ HackneyRepairs/Controllers/WorkOrdersController.cs
@@ -10,54 +10,54 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HackneyRepairs.Controllers
 {
-	[Produces("application/json")]
+    [Produces("application/json")]
     [Route("v1/workorders")]
-	public class WorkOrdersController : Controller
+    public class WorkOrdersController : Controller
     {
-		private IHackneyWorkOrdersService _workOrdersService;
-		private ILoggerAdapter<WorkOrdersActions> _loggerAdapter;
+        private IHackneyWorkOrdersService _workOrdersService;
+	private ILoggerAdapter<WorkOrdersActions> _loggerAdapter;
 
-		public WorkOrdersController(ILoggerAdapter<WorkOrdersActions> loggerAdapter, IUhtRepository uhtRepository)
+	public WorkOrdersController(ILoggerAdapter<WorkOrdersActions> loggerAdapter, IUhtRepository uhtRepository)
         {
-			_loggerAdapter = loggerAdapter;
-			var factory = new HackneyWorkOrdersServiceFactory();
-			_workOrdersService = factory.build(uhtRepository, _loggerAdapter);
+	    _loggerAdapter = loggerAdapter;
+	    var factory = new HackneyWorkOrdersServiceFactory();
+	    _workOrdersService = factory.build(uhtRepository, _loggerAdapter);
         }
         
-		[HttpGet("{workOrderReference}")]
-		public async Task<JsonResult> Get(string workOrderReference)
+	    [HttpGet("{workOrderReference}")]
+	    public async Task<JsonResult> Get(string workOrderReference)
+	    {
+		var workOrdersActions = new WorkOrdersActions(_workOrdersService, _loggerAdapter);
+		WorkOrderEntity result = new WorkOrderEntity();
+		try
 		{
-			var workOrdersActions = new WorkOrdersActions(_workOrdersService, _loggerAdapter);
-			WorkOrderEntity result = new WorkOrderEntity();
-			try
-			{
-				result = await workOrdersActions.GetWorkOrderByReference(workOrderReference);
-				var json = Json(result);
-                json.StatusCode = 200;
-                return json;
-			}
-			catch (MissingWorkOrderException ex)
-			{
-				var error = new ApiErrorMessage
-				{
-					developerMessage = ex.Message,
-					userMessage = @"Cannot find work order."
-				};
-				var jsonResponse = Json(error);
-                jsonResponse.StatusCode = 404;
-				return jsonResponse;
-			}
-			catch (UhtRepositoryException ex)
-			{
-				var error = new ApiErrorMessage
+		    result = await workOrdersActions.GetWorkOrderByReference(workOrderReference);
+		    var json = Json(result);
+                    json.StatusCode = 200;
+                    return json;
+		}
+		catch (MissingWorkOrderException ex)
+		{
+		    var error = new ApiErrorMessage
+		    {
+		        developerMessage = ex.Message,
+			userMessage = @"Cannot find work order."
+		    };
+		    var jsonResponse = Json(error);
+                    jsonResponse.StatusCode = 404;
+	       	    return jsonResponse;
+		}
+		catch (UhtRepositoryException ex)
+		{
+		    var error = new ApiErrorMessage
                 {
                     developerMessage = ex.Message,
                     userMessage = @"We had issues with connecting to the data source."
                 };
-				var jsonResponse = Json(error);
+		var jsonResponse = Json(error);
                 jsonResponse.StatusCode = 500;
                 return jsonResponse;
-			}
-		}
+	    }
+	}
     }
 }
