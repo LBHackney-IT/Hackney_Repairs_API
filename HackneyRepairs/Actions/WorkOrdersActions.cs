0f597b8e9fa997230d7f4e2719df5c8899a7d2c4
LBH-fschultesz <ferenc.schultesz@hackney.gov.uk>
2018-08-01 08:51:40 +0100

    Update WorkOrdersActions.cs
    Fixed indentation issues

--- HackneyRepairs/Actions/WorkOrdersActions.cs
+++ HackneyRepairs/Actions/WorkOrdersActions.cs
@@ -8,28 +8,27 @@ namespace HackneyRepairs.Actions
 {
     public class WorkOrdersActions
     {
-		IHackneyWorkOrdersService _workOrdersService;
-		private readonly ILoggerAdapter<WorkOrdersActions> _logger;
+	IHackneyWorkOrdersService _workOrdersService;
+	private readonly ILoggerAdapter<WorkOrdersActions> _logger;
 
-		public WorkOrdersActions(IHackneyWorkOrdersService workOrdersService, ILoggerAdapter<WorkOrdersActions> logger)
+	public WorkOrdersActions(IHackneyWorkOrdersService workOrdersService, ILoggerAdapter<WorkOrdersActions> logger)
         {
-			_workOrdersService = workOrdersService;
-			_logger = logger;
+	    _workOrdersService = workOrdersService;
+	    _logger = logger;
         }
 
-		public async Task<WorkOrderEntity> GetWorkOrderByReference(string workOrderReference)
-		{
-			_logger.LogInformation($"Finding work order details for reference: {workOrderReference}");
-		    var result = await _workOrdersService.GetWorkOrderByReference(workOrderReference);            
-            if (result == null)
-			{
-				_logger.LogError($"Work order not found for reference: {workOrderReference}");
-				throw new MissingWorkOrderException();
-			}
-			_logger.LogInformation($"Work order details returned for: {workOrderReference}");
-			return result;
-		}
+        public async Task<WorkOrderEntity> GetWorkOrderByReference(string workOrderReference)
+        {
+	    _logger.LogInformation($"Finding work order details for reference: {workOrderReference}");
+	    var result = await _workOrdersService.GetWorkOrderByReference(workOrderReference);            
+	    if (result == null)
+	    {
+	        _logger.LogError($"Work order not found for reference: {workOrderReference}");
+	        throw new MissingWorkOrderException();
+	    }
+	    _logger.LogInformation($"Work order details returned for: {workOrderReference}");
+	    return result;
+        }
     }
-
     public class MissingWorkOrderException : System.Exception { }
 }
