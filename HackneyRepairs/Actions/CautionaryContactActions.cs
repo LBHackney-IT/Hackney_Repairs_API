949d0c488d060a991f084cb9c93b8d301bddc1bb
VladyslavAtamanyuk <Vladyslav.Atamanyuk@hackney.gov.uk>
2019-04-11 15:44:04 +0100

    Built out Controller and actions classes
    Need to start tests

---
+++ HackneyRepairs/Actions/CautionaryContactActions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackneyRepairs.Actions
+{
+    public class CautionaryContactActions
+    {
+        public async Task<object> GetCautionaryContactByFirstLineOfAddress()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HackneyRepairs/Controllers/CautionaryContactController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HackneyRepairs.Actions;
+using HackneyRepairs.Builders;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackneyRepairs.Controllers
+{
+    public class CautionaryContactController : Controller
+    {
+        [HttpGet]
+        public async Task<JsonResult> GetCautionaryContactByFirstLineOfAddress()
+        {
+            CautionaryContactActions actions = new CautionaryContactActions();
+            var result = await actions.GetCautionaryContactByFirstLineOfAddress();
+            return ResponseBuilder.Ok(result);
+        }
+    }
+}
