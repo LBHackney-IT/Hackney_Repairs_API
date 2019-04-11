d384286b8fa939c0550d11eaa019fda73c3677d0
VladyslavAtamanyuk <Vladyslav.Atamanyuk@hackney.gov.uk>
2019-04-11 16:38:19 +0100

    Started test, more on actions and controller
    Started the creation of the interfaces and added more to the actions and controller classes

--- HackneyRepairs/Controllers/CautionaryContactController.cs
+++ HackneyRepairs/Controllers/CautionaryContactController.cs
@@ -1,19 +1,35 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using HackneyRepairs.Actions;
 using HackneyRepairs.Builders;
+using HackneyRepairs.Factories;
+using HackneyRepairs.Interfaces;
+using HackneyRepairs.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HackneyRepairs.Controllers
 {
     public class CautionaryContactController : Controller
     {
+        private IHackneyCautionaryContactService _cautionaryContactService;
+        private IHackneyCautionaryContactRequestBuilder _cautionaryContactRequestBuilder;
+        private ILoggerAdapter<CautionaryContactActions> _cautionaryContactLoggerAdapter;
+        private HackneyConfigurationBuilder _configBuilder;
+
+        public CautionaryContactController(IHackneyCautionaryContactService _cautionaryContactService, IHackneyCautionaryContactRequestBuilder _cautionaryContactRequestBuilder, ILoggerAdapter<CautionaryContactActions> _cautionaryContactLoggerAdapter)
+        {
+            HackneyCautionaryContactServiceFactory cautionaryContactFactory = new HackneyCautionaryContactServiceFactory();
+            _configBuilder = new HackneyConfigurationBuilder((Hashtable)Environment.GetEnvironmentVariables(), ConfigurationManager.AppSettings);
+        }
+
         [HttpGet]
         public async Task<JsonResult> GetCautionaryContactByFirstLineOfAddress()
         {
-            CautionaryContactActions actions = new CautionaryContactActions();
+            CautionaryContactActions actions = new CautionaryContactActions(_cautionaryContactService, _cautionaryContactRequestBuilder, _cautionaryContactLoggerAdapter);
             var result = await actions.GetCautionaryContactByFirstLineOfAddress();
             return ResponseBuilder.Ok(result);
         }
---
+++ HackneyRepairs/Tests/Actions/CautionaryContactActionsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HackneyRepairs.Tests.Actions
+{
+    public class CautionaryContactActionsTests
+    {
+        #region Cautionary Contact Data by First Line of Address
+        //[Fact]
+        //public async Task get_cautionary_contacts_by_first_line_of_address()
+        //{
+            
+        //}
+        #endregion
+    }
+}
