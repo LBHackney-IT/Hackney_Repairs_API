b7170ceeb372b28dedfe0f0050c7647642b15c3b
VladyslavAtamanyuk <Vladyslav.Atamanyuk@hackney.gov.uk>
2019-04-16 16:42:20 +0100

    Completed the end point (with tests failing)

--- HackneyRepairs/Services/FakeCautionaryContactService.cs
+++ HackneyRepairs/Services/FakeCautionaryContactService.cs
@@ -9,9 +9,43 @@ namespace HackneyRepairs.Services
 {
     public class FakeCautionaryContactService : IHackneyCautionaryContactService
     {
-        public Task<CautionaryContactLevelModel[]> GetCautionaryContactByRef(string firstLineOfAddress)
+        public Task<CautionaryContactLevelModel[]> GetCautionaryContactByRef(string reference)
         {
-            throw new NotImplementedException();
+            var CautionaryContactList = new CautionaryContactLevelModel[2];
+            CautionaryContactLevelModel[] emptyCautionaryContactList;
+            var cautionaryContact1 = new CautionaryContactLevelModel()
+            {
+                PropertyReference = "00000123",
+                ContactNo = 111111,
+                Title = "MRS",
+                Forenames = "BLIN",
+                Surename = "",
+                CallerNotes = "Don't come its not Healthy",
+                alertCode = "CX"
+            };
+            var cautionaryContact2 = new CautionaryContactLevelModel()
+            {
+                PropertyReference = "00000123",
+                ContactNo = 111111,
+                Title = "MRS",
+                Forenames = "BLIN",
+                Surename = "",
+                CallerNotes = "Don't come its not Healthy",
+                alertCode = "CX"
+            };
+            CautionaryContactList[0] = cautionaryContact1;
+            CautionaryContactList[1] = cautionaryContact2;
+            switch (reference)
+            {
+                case "Acacia":
+                    return Task.Run(() => CautionaryContactList);
+                case "Elmbridge":
+                    emptyCautionaryContactList = null;
+                    return Task.Run(() => emptyCautionaryContactList);
+                default:
+                    emptyCautionaryContactList = new CautionaryContactLevelModel[0];
+                    return Task.Run(() => emptyCautionaryContactList);
+            }
         }
     }
 }
---
+++ HackneyRepairs/Tests/Integration/CautionaryContactIntegrationTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HackneyRepairs.Tests.Integration
+{
+    public class CautionaryContactShould
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+        public CautionaryContactShould()
+        {
+            Environment.SetEnvironmentVariable("UhwDb", "connectionString=Test");
+            _server = new TestServer(new WebHostBuilder()
+            .UseStartup<TestStartup>());
+            _client = _server.CreateClient();
+        }
+
+        #region GET Cautionary Contact By Property Reference
+        [Fact]
+        public async Task return_a_200_result_for_valid_request_by_reference()
+        {
+            var result = await _client.GetAsync("v1/cautionary_contact/?reference=52525252");
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+            Assert.Equal("application/json", result.Content.Headers.ContentType.MediaType);
+        }
+
+        [Fact]
+        public async Task return_a_empty_object_result_when_there_is_no_cautionary_contact_found_for_the_reference()
+        {
+            var result = await _client.GetAsync("v1/cautionary_contact/?reference=52525252");
+            string resultString = await result.Content.ReadAsStringAsync();
+            StringBuilder json = new StringBuilder();
+            json.Append("{");
+            json.Append("\"results\":[]\"");
+            Assert.Equal(json.ToString(), resultString);
+        }
+
+        [Fact]
+        public async Task return_a_json_object_for_valid_reference()
+        {
+            var result = await _client.GetAsync("v1/cautionary_contact/?reference=52525252");
+            string resultString = await result.Content.ReadAsStringAsync();
+            StringBuilder json = new StringBuilder();
+            json.Append("{");
+            json.Append("\"propertyReference\":\"52525252\",");
+            json.Append("\"contactNo\":\"111111\",");
+            json.Append("\"title\":\"MRS\",");
+            json.Append("\"forenames\":\"BLIN\",");
+            json.Append("\"surename\":\"null\",");
+            json.Append("\"callerNotes\":\"Don't come here, weapons found in the property\",");
+            json.Append("\"alertCode\":CX");
+            json.Append("}");
+            Assert.Equal(json.ToString(), resultString);
+        }
+        #endregion
+    }
+}
