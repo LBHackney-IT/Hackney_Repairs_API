025188a69cb2ab873289a1b06a5000338b5fde25
LBHBomotoso <46450498+LBHBomotoso@users.noreply.github.com>
2019-10-15 14:45:53 +0100

    Check timezone and current date

--- HackneyRepairs/Formatters/DateTimeFormatter.cs
+++ HackneyRepairs/Formatters/DateTimeFormatter.cs
@@ -10,10 +10,13 @@ namespace HackneyRepairs.Formatters
         public static string FormatDateTimeToGMT(DateTime date)
         {
             TimeSpan ts = TimeZoneInfo.Local.GetUtcOffset(date);
-            TimeSpan bts = TimeZoneInfo.Local.BaseUtcOffset;
 
-            //compare the UTC offsets
-            return (ts > bts) ? date.ToString("s") + "Z" : date.Add(ts).ToString("s") + "Z";
+            //AWS Cloud TimeZone
+            DateTime dayLightSavingDate = new DateTime(DateTime.Now.Year, 10, 27);
+            if (TimeZoneInfo.Local.Id.Equals("UTC") && (DateTime.Now < dayLightSavingDate))
+                return date.AddHours(1).ToString("s") + "Z";
+
+            return date.ToString("s") + "Z";
         }
     }
 }
