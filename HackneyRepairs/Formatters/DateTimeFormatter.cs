49dc91b3a04e8e70bee0955616906f3463a7b53d
LBHBomotoso <46450498+LBHBomotoso@users.noreply.github.com>
2019-10-15 15:13:28 +0100

    removed unused line

--- HackneyRepairs/Formatters/DateTimeFormatter.cs
+++ HackneyRepairs/Formatters/DateTimeFormatter.cs
@@ -9,8 +9,6 @@ namespace HackneyRepairs.Formatters
     {
         public static string FormatDateTimeToGMT(DateTime date)
         {
-            TimeSpan ts = TimeZoneInfo.Local.GetUtcOffset(date);
-
             //AWS Cloud TimeZone
             DateTime dayLightSavingDate = new DateTime(DateTime.Now.Year, 10, 27);
             if (TimeZoneInfo.Local.Id.Equals("UTC") && (DateTime.Now < dayLightSavingDate))
